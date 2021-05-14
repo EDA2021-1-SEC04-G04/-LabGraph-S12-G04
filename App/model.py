eb48a214b13edd632271fd971eedb053beb3fcfc
JackRakham <j.villatet@uniandes.edu.co>
2021-05-14 11:45:53 -0500

    hola

--- App/model.py
+++ App/model.py
@@ -23,6 +23,8 @@
  * Dario Correal
  *
  """
+import time
+import tracemalloc
 import config
 from DISClib.ADT.graph import gr
 from DISClib.ADT import map as m
@@ -285,3 +287,31 @@ def compareroutes(route1, route2):
         return 1
     else:
         return -1
+#Funciones tiempo
+def getTime():
+    """
+    devuelve el instante tiempo de procesamiento en milisegundos
+    """
+    return float(time.perf_counter()*1000)
+
+def getMemory():
+    """
+    toma una muestra de la memoria alocada en instante de tiempo
+    """
+    return tracemalloc.take_snapshot()
+
+def deltaMemory(start_memory, stop_memory):
+    """
+    calcula la diferencia en memoria alocada del programa entre dos
+    instantes de tiempo y devuelve el resultado en bytes (ej.: 2100.0 B)
+    """
+    memory_diff = stop_memory.compare_to(start_memory, "filename")
+    delta_memory = 0.0
+
+    # suma de las diferencias en uso de memoria
+    for stat in memory_diff:
+        delta_memory = delta_memory + stat.size_diff
+    # de Byte -> kByte
+    delta_memory = delta_memory/1024.0
+    return delta_memory
+
